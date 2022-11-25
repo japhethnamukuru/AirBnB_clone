7febc3c281f1efd63237969bbe9b213f710aea04
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-11-25 11:05:47 -0800

    updated base model tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,12 +3,30 @@
 """This module tests the base model class"""
 from models.base_model import BaseModel
 import unittest
+from datetime import datetime
+import re
 
 
 class TestBaseModel(unittest.TestCase):
     def setUp(self):
         self.obj = BaseModel()
+        self.obj1 = BaseModel()
 
-    # testing instances
-    def test_id(self):
-        self.assertIs(type(self.obj.id), str)
+    # testing instance attributes
+    def test_instance_id(self):
+        obj = self.obj
+        obj1 = self.obj1
+        self.assertIsInstance(obj, BaseModel)
+        self.assertIsInstance(obj.id, str)
+        self.assertNotEqual(obj, obj1)
+        match = re.fullmatch(r'\w{8}-\w{4}-\w{4}-\w{4}-\w{12}', obj.id)
+        self.assertTrue(match)
+
+    def test_instance_created_at(self):
+        obj = self.obj
+        self.assertIsInstance(obj.created_at, datetime)
+
+    def test_instance_updated_at(self):
+        obj = self.obj
+        self.assertIsInstance(obj.updated_at, datetime)
+        self.assertEqual(obj.created_at, obj.updated_at)
