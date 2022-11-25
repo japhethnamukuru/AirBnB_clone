13affc3216b1b911e3e81cfa3abfecfa3468481b
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-11-25 07:15:04 -0800

    base model file

---
+++ models/base_model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+    Python OOP, the airbnb clone project. This module contains the base class for other models
+"""
+
+
+class BaseModel:
+    """The base model for all the other classes"""
+
+    id = ""
