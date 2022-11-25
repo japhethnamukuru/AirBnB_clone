ab808d25a2aea6bcb56f75cddd84931ef56bb3c2
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-11-25 07:57:21 -0800

    modified base model file

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,21 @@
 """
     Python OOP, the airbnb clone project. This module contains the base class for other models
 """
+import uuid
+import datetime
 
 
 class BaseModel:
     """The base model for all the other classes"""
 
-    id = ""
+    def __init__(self, id, created_at, update_at):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datime.now().isoformat()
+        self.updated_at = datetime.datetime.now().isoformat()
+
+
+    def __str__(self):
+        return "[{}] ({}) ({})".format(self.__class__.__name, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.datetime.now().isformat()
