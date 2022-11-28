6a161f04ff3e7f6c7445d9f224234d63fdea7ff0
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-11-28 08:55:19 -0800

    modified consoe file

--- console.py
+++ console.py
@@ -5,5 +5,12 @@
 """
 import cmd
 
+
+class HBNBCommand(Cmd.cmd):
+    """
+        Inherits from the Cmd super class from the cmd module
+    """
+    pass
+
 if __name__ = '__main__':
     HBNBCommand().cmdloop()
