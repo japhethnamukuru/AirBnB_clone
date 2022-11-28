7e58a4eb58c9afea174e98007f6802beb172e069
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-11-28 10:06:47 -0800

    update the file_storage engine to handle user model

--- models/__init__.py
+++ models/__init__.py
@@ -5,8 +5,9 @@
 """
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models.user import User
 
-test_classes = {'BaseModel': BaseModel}
+test_classes = {'BaseModel': BaseModel, "User": User}
 
 storage = FileStorage()
 storage.reload()
